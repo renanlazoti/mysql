8491567272619f7a655709db22656a061dd46c85
Renan Lazoti <87093301+renanlazoti@users.noreply.github.com>
2022-05-16 16:37:21 -0300

    First Commit mysql

---
+++ agenda.sql
@@ -0,0 +1,17 @@
+/**
+	Agenda de contatos 
+    @author Renan Lazoti
+*/
+
+-- Verificar banco de dados
+show databases;
+
+-- Criar um novo banco de dados
+create database teste;
+create database agendarenan;
+
+-- Deletar um banco de dados
+drop database teste;
+
+-- Selecionar um banco de dados
+use agendarenan;
